7bbd617fe3f5407b98440ca5da6e71fd0b1cfffc
xiaoyannn <1687028512@qq.com>
2020-08-15 00:15:04 +0800

    init

---
+++ build/precache-manifest.0d8b7a6685f972c50780f4833d3b51ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34add4ce84848454fc7004825636e895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43de73555e32eae7ef86",
+    "url": "/static/js/2.640365a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.640365a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd29baf9c9479f5dcbbf",
+    "url": "/static/js/main.efea54f8.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
